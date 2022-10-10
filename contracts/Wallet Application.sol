392400daaeb7bdfe9124daa090e0ca62b3cbeec4
Remix Workspace <>
2022-10-10 14:38:55 +0100

    initial commit

---
+++ contracts/Wallet Application.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+contract FeeCollector {
+    address public owner;
+    uint256 public balance;
+
+    modifier onlyOwner
+    {
+        require(owner == msg.sender);
+        _;
+    }
+    constructor() {
+        owner= msg.sender;
+    }
+
+    receive()payable external {
+        balance += msg.value;
+    }
+
+    function withdraw (uint amount, address payable destAddr) public{
+        require (msg.sender == owner, "only owner can withdraw");
+        require (amount <= balance, "Insufficient funds");
+
+        destAddr.transfer(amount);
+        balance -= amount;
+    }
+}
